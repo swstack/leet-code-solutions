7d5c9d29a89dfa94af95a26687ff6c4b5265e955
Stephen Stack <sstack@punchthrough.com>
2017-01-14 15:28:20 -0800

    zigzag: skeleton w/ first valid test

---
+++ solutions/zigzag_conversion.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def convert(self, s, numRows):
+        """
+        :type s: str
+        :type numRows: int
+        :rtype: str
+        """
+
+
+# Valid: 3 rows
+solution = Solution().convert("PAYPALISHIRING", 3)
+assert solution == "PAHNAPLSIIGYIR"
