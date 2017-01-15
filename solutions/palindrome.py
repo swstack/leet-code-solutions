43496f11e2c4c068e9ab1ae816373ce6915ddc99
Stephen Stack <sstack@punchthrough.com>
2017-01-15 10:14:15 -0800

    palindrome: skeleton and instructions

---
+++ solutions/palindrome.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        Determine whether an integer is a palindrome. Do this without extra space.
+
+        Definition: a word, phrase, or sequence that reads the same backward as forward,
+        e.g., madam or nurses run.
+
+        :type x: int
+        :rtype: bool
+        """
