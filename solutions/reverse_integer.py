878de2c6250443d75e2e03be89f288c9da070c3c
Stephen Stack <sstack@punchthrough.com>
2017-01-14 18:15:21 -0800

    reverse-int: first solution casting/reversing string

--- solutions/reverse_integer.py
+++ solutions/reverse_integer.py
@@ -5,11 +5,23 @@ class Solution(object):
         :rtype: int
         """
 
+        is_negative = True if x < 0 else False
+
+        x_str = str(abs(x))
+        reversed = ''
+        for c in x_str:
+            reversed = c + reversed
+
+        x_int = int(reversed)
+        solution = -x_int if is_negative else x_int
+        print(solution)
+        return solution
+
 
 # Valid: positive
-solution = Solution().convert(321)
+solution = Solution().reverse(321)
 assert solution == 123
 
 # Valid: negative
-solution = Solution().convert(-321)
+solution = Solution().reverse(-321)
 assert solution == -123
