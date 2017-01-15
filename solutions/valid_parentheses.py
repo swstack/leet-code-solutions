c691d75bf3932d1aed12eb42ad13c713911e2fb9
Stephen Stack <sstack@punchthrough.com>
2017-01-15 10:17:04 -0800

    valid-parentheses: skeleton and instructions

---
+++ solutions/valid_parentheses.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine
+        if the input string is valid.
+
+        The brackets must close in the correct order, "()" and "()[]{}" are all valid but
+        "(]" and "([)]" are not.
+
+        :type s: str
+        :rtype: bool
+        """
