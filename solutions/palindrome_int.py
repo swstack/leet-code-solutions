ca6b2b296037e91068f99fe565b8757d038f997f
Stephen Stack <sstack@punchthrough.com>
2017-02-08 16:36:15 -0600

    palindrome-int: working non-recursive solution

--- solutions/palindrome_int.py
+++ solutions/palindrome_int.py
@@ -1,4 +1,27 @@
 class Solution(object):
+    def reverse_int(self, x):
+
+        negative = False
+        if x < 0:
+            negative = True
+            x = abs(x)
+
+        if x < 10:
+            return -x if negative else x
+
+        reversed = x % 10
+        first_digits = int(x / 10)
+
+        while first_digits >= 10:
+            next_last_digit = first_digits % 10
+            first_digits = int(first_digits / 10)
+            reversed = (reversed * 10) + next_last_digit
+
+        next_last_digit = first_digits % 10
+        reversed = (reversed * 10) + next_last_digit
+
+        return -reversed if negative else reversed
+
     def isPalindrome(self, x):
         """
         Determine whether an integer is a palindrome. Do this without extra space.
@@ -10,21 +33,30 @@ class Solution(object):
         :rtype: bool
         """
 
+        reversed = self.reverse_int(x)
+        return reversed == x
 
 
+assert Solution().reverse_int(1234) == 4321
+assert Solution().reverse_int(11) == 11
+assert Solution().reverse_int(41) == 14
+assert Solution().reverse_int(1) == 1
+assert Solution().reverse_int(0) == 0
+assert Solution().reverse_int(-1) == -1
+assert Solution().reverse_int(-134) == -431
 
-# Valid: 3 digit
-solution = Solution().isPalindrome(123)
-assert solution == 321
 
-# Valid: 2 digit
-solution = Solution().isPalindrome(23)
-assert solution == True
+# Valid: not palindrome
+assert Solution().isPalindrome(1243) is False
 
-# Valid: 1 digit
-solution = Solution().isPalindrome(1)
-assert solution == True
+# Valid: is palindrome
+assert Solution().isPalindrome(1331) is True
 
-# Valid: duplicates
-solution = Solution().isPalindrome(11)
-assert solution == True
+# Valid: is palindrome
+assert Solution().isPalindrome(131) is True
+
+# Valid: duplicates, palindrome
+assert Solution().isPalindrome(11) is True
+
+# Valid: negative
+assert Solution().isPalindrome(-2147483648) is False
