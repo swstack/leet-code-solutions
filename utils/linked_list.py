78f0843b633011ce5bdf264bf6dabf878acf194e
Stephen Stack <sstack@punchthrough.com>
2017-02-13 00:09:59 -0600

    util: abstract LinkedList into class

--- utils/linked_list.py
+++ utils/linked_list.py
@@ -4,29 +4,44 @@ class ListNode(object):
         self.next = None
 
 
-def print_list(head):
+class LinkedList(object):
 
-    while head.next is not None:
-        print(head.val)
-        head = head.next
+    @classmethod
+    def from_array(cls, data):
+        first_node = None
+        previous_node = None
+        for i in data:
+            n = ListNode(i)
 
-    print(head.val)
+            if previous_node:
+                previous_node.next = n
+
+            if first_node is None:
+                first_node = n
+
+            previous_node = n
+
+        return cls(first_node)
+
+    def __init__(self, head):
+        self.head = head
+
+    def __str__(self):
+
+        out = ""
+        head = self.head
+        while head.next is not None:
+            out += "{} -> ".format(head.val)
+            head = head.next
+
+        out += "{}".format(head.val)
+        return out
+
+    def __eq__(self, other):
+        pass
 
 
-def populate(data):
-    """Returns the head of a linked list"""
-
-    first_node = None
-    previous_node = None
-    for i in data:
-        n = ListNode(i)
-
-        if previous_node:
-            previous_node.next = n
-
-        if first_node is None:
-            first_node = n
-
-        previous_node = n
-
-    return first_node
+if __name__ == '__main__':
+    ll = LinkedList.from_array([1, 2, 3])
+    print(ll)
+    print(ll)
