909dd8a3d332517d0f3dd12f251f98dbd49f0ab5
Stephen Stack <sstack@punchthrough.com>
2017-02-13 00:29:28 -0600

    util: create equality method for LinkedList

--- utils/linked_list.py
+++ utils/linked_list.py
@@ -27,21 +27,52 @@ class LinkedList(object):
         self.head = head
 
     def __str__(self):
-
         out = ""
-        head = self.head
-        while head.next is not None:
-            out += "{} -> ".format(head.val)
-            head = head.next
+        _head = self.head
+        while _head.next is not None:
+            out += "{} -> ".format(_head.val)
+            _head = _head.next
 
-        out += "{}".format(head.val)
+        out += "{}".format(_head.val)
         return out
 
     def __eq__(self, other):
-        pass
+        if not other:
+            return False
+
+        _head1 = self.head
+        _head2 = other.head
+
+        if _head1.val != _head2.val:
+            return False
+
+        while _head1.next is not None:
+            if _head2.next is None:
+                return False
+
+            _head1 = _head1.next
+            _head2 = _head2.next
+
+            if _head1.val != _head2.val:
+                return False
+
+        if _head2.next is not None:
+            return False
+
+        return True
 
 
 if __name__ == '__main__':
     ll = LinkedList.from_array([1, 2, 3])
     print(ll)
     print(ll)
+    print(LinkedList.from_array([5]))
+
+    ll1 = LinkedList.from_array([1, 2, 3])
+    ll2 = LinkedList.from_array([1, 2, 3])
+    assert ll1 == ll1
+    assert ll1 == ll2 == ll
+
+    ll1 = LinkedList.from_array([1, 2])
+    ll2 = LinkedList.from_array([1, 2, 3])
+    assert ll1 != ll2
